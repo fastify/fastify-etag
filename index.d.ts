4912eeab8d53da7a9d6f9652efd13ffe8c5e8c59
Joel Mukuthu <joelmukuthu@gmail.com>
2020-04-22 13:32:11 +0200

    Fix TypeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,10 @@ import { Plugin } from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 
 interface FastifyEtagOptions {
-  algorithm: 'fnv1a' | string;
+  algorithm?: 'fnv1a' | string;
 }
 
-const fastifyEtag: Plugin<
+declare const fastifyEtag: Plugin<
   Server,
   IncomingMessage,
   ServerResponse,
