7017ba1efb3028d30bf03a1c0544f3b0e44cfdb8
uzlopak <aras.abbasi@googlemail.com>
2022-11-28 13:12:26 +0100

    nodenext compatibility

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,16 @@
 import { FastifyPluginAsync } from 'fastify'
 
-export interface FastifyEtagOptions {
-  algorithm?: 'fnv1a' | string;
-  weak?: boolean;
+type FastifyEtag = FastifyPluginAsync<fastifyEtag.FastifyEtagOptions>
+
+declare namespace fastifyEtag {
+  export interface FastifyEtagOptions {
+    algorithm?: 'fnv1a' | string;
+    weak?: boolean;
+  }
+
+  export const fastifyEtag: FastifyEtag
+  export { fastifyEtag as default }
 }
 
-declare const fastifyEtag: FastifyPluginAsync<FastifyEtagOptions>
-
-export default fastifyEtag;
+declare function fastifyEtag(...params: Parameters<FastifyEtag>): ReturnType<FastifyEtag>
+export = fastifyEtag
