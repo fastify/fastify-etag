9eccf40241743d91d1ca3cd3d3137024e7b5588e
liqiang <974923609@qq.com>
2024-04-22 21:28:14 +0800

    weak etag match support (#109)

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function fastifyEtag (app, { algorithm, weak, replyWith304 = true }) {
       reply.header('etag', etag)
     }
 
-    if (replyWith304 && req.headers['if-none-match'] === etag) {
+    if (replyWith304 && (req.headers['if-none-match'] === etag || req.headers['if-none-match'] === 'W/' + etag || 'W/' + req.headers['if-none-match'] === etag)) {
       reply.code(304)
       newPayload = ''
     }
